bc17b2d664d2696a20b895170e925012e26b727f
Alexandre Blanchet <ab@ovalo.fr>
2024-09-19 18:17:32 +0200

    change redirect url

--- src/clientConfig.tsx
+++ src/clientConfig.tsx
@@ -1,5 +1,5 @@
 export const clientConfig = {
     clientId: 'd7fcb9e0-08c6-458e-9559-481ba326c996',
-    redirectUri: 'http://localhost:3000/',
+    redirectUri: 'https://alexandreblanchet.github.io/custom-skills-manager/',
     offlinePresenceId: 'ccf3c10a-aa2c-4845-8e8d-f59fa48c58e5',
 }
