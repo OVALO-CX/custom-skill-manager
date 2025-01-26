b3e5781c705cc8e8b9193d516dbce49cd80452cb
Alexandre Blanchet <ab@ovalo.fr>
2025-01-26 12:44:53 +0100

    upate client id

--- src/clientConfig.tsx
+++ src/clientConfig.tsx
@@ -1,7 +1,7 @@
 
 export const clientConfig = {
-    clientId: 'd7fcb9e0-08c6-458e-9559-481ba326c996',
-    redirectUri: 'https://alexandreblanchet.github.io/custom-skills-manager/',
+    clientId: '8b2daf9c-f61f-4951-9f34-48790cc52701',
+    redirectUri: 'https://adminovalo.github.io/custom-skill-manager/',
     //redirectUri: 'http://localhost:3000/',
     offlinePresenceId: 'ccf3c10a-aa2c-4845-8e8d-f59fa48c58e5',
 }
