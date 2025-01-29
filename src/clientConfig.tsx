029bc8ec0ff8936a5153e9728c96e98415d354c0
Alexandre Blanchet <ab@ovalo.fr>
2025-01-29 14:44:21 +0100

    change redirect uri

--- src/clientConfig.tsx
+++ src/clientConfig.tsx
@@ -1,7 +1,7 @@
 
 export const clientConfig = {
     clientId: '8b2daf9c-f61f-4951-9f34-48790cc52701',
-    redirectUri: 'https://adminovalo.github.io/custom-skill-manager/',
+    redirectUri: 'https://ovalo-cx.github.io/custom-skill-manager/',
     //redirectUri: 'http://localhost:3000/',
     offlinePresenceId: 'ccf3c10a-aa2c-4845-8e8d-f59fa48c58e5',
 }
